f981d78d04ac081f3aa27e90154620ff42a58009
yuanpoint <2548131226@qq.com>
2017-08-16 10:31:51 +0800

    Update Config.php

--- Config.php
+++ Config.php
@@ -7,22 +7,22 @@ namespace Yiwangtong;
 
 
 //商户分行号
-define('BRANCHNO','0571');
+define('BRANCHNO','');
 
 //商户商户号
-define('MERCHANTNO','001276');
+define('MERCHANTNO','');
 
 //成功支付结果通知地址，商户接收成功支付结果通知的地址
-define('PAYNOTICEURL', 'http://192.168.1.104:85/index.php/Api/Index/yiwantong_url');
+define('PAYNOTICEURL', ');
 
 //成功签约结果通知地址
-define('SIGNNOTICEURL', 'http://192.168.1.104:85/index.php/Api/Index/yiwangtong_urls');
+define('SIGNNOTICEURL', '');
 
 //支付秘钥sMerchantKey
-define('SMERCHANTKEY','123456abcdeABCDE');//测试环境
+define('SMERCHANTKEY','');//测试环境
 
 //商户接收成功签约结果通知的地址
-define('NOTICEURL', 'http://192.168.1.104:85/index.php/Api/Index/yiwangtong_urls');
+define('NOTICEURL', '');
 
 
 
